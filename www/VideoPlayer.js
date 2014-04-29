b60186d4c5f8dec3f54de3f6b46e6f6e2943dc29
peter hannon <peter.f.hannon@gmail.com>
2014-04-29 15:17:43 +0100

    tidying

--- www/VideoPlayer.js
+++ www/VideoPlayer.js
@@ -1,16 +1,4 @@
-cordova.define("com.dawsonloudon.videoplayer.VideoPlayer", function(require, exports, module) { /*-
- * cordova VideoPlayer Plugin for Android
- *
- * Created by Simon MacDonald (2008) MIT Licensed
- * Revised for Cordova 3.3+ by Dawson Loudon (2013) MIT Licensed
- *
- * Usages:
- *
- * VideoPlayer.play("http://path.to.my/video.mp4");
- * VideoPlayer.play("file:///path/to/my/video.mp4");
- * VideoPlayer.play("file:///android_asset/www/path/to/my/video.mp4");
- * VideoPlayer.play("https://www.youtube.com/watch?v=en_sVVjWFKk");
- */
+cordova.define("com.unit11apps.videoplayer.VideoPlayer", function(require, exports, module) {
 
 var exec = require("cordova/exec");
 
@@ -21,6 +9,5 @@ var VideoPlayer = {
 };
 
 module.exports = VideoPlayer;
-
 
 });
